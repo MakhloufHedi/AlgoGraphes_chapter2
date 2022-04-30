392e5a1273a69fcf0ff5c4b61bc95a11a29b159b
MohamedBenAhmed55 <73948600+MohamedBenAhmed55@users.noreply.github.com>
2022-04-30 14:55:46 +0100

    crosswords updated

--- src/Components/Chapters/Chapter1/crosswords.js
+++ src/Components/Chapters/Chapter1/crosswords.js
@@ -15,65 +15,78 @@ const data = {
     across: {
         1: {
             clue: "abbreviated programming language",
-            answer: "JS",
-            row: 1,
-            col: 6
+            answer: "articulation",
+            row: 3,
+            col: 0
         },
         2: {
             clue: "JS library for building UIs",
-            answer: "REACT",
-            row: 3,
-            col: 6
+            answer: "degre",
+            row: 6,
+            col: 7
         },
         3: {
-            clue: "DOM bindings for React Router",
-            answer: "ROUTERDOM",
-            row: 5,
-            col: 0
+            clue: "JS library for building UIs",
+            answer: "suivant",
+            row: 8,
+            col: 3
         },
         4: {
-            clue: "JS runtime environment",
-            answer: "NODE",
+            clue: "DOM bindings for React Router",
+            answer: "precedant",
             row: 10,
-            col: 2
-        }
-    },
-    down: {
+            col: 5
+        },
         5: {
-            clue: "content management system",
-            answer: "WORDPRESS",
-            row: 0,
+            clue: "Un arc dont la supression augmente le nombre des comosantes connexes",
+            answer: "incidence",
+            row: 12,
             col: 0
         },
         6: {
-            clue: "enable config override",
-            answer: "REWIRED",
-            row: 4,
-            col: 4
-        },
+            clue: "Un arc dont la supression augmente le nombre des comosantes connexes",
+            answer: "isthme",
+            row: 15,
+            col: 8
+        }
+    },
+    down: {
         7: {
-            clue: "crossword creator",
-            answer: "JARED",
-            row: 1,
-            col: 6
+            clue: "Un graphe qui possede une chaine entre 2 sommets quelconques",
+            answer: "Connexe",
+            row: 0,
+            col: 11
         },
         8: {
-            clue: "online publishing platform",
-            answer: "MEDIUM",
+            clue: "enable config override",
+            answer: "adjacence",
             row: 5,
-            col: 8
+            col: 7
         },
         9: {
-            clue: "favourite code editor",
-            answer: "ATOM",
-            row: 2,
-            col: 10
+            clue: "crossword creator",
+            answer: "symetrique",
+            row: 6,
+            col: 13
         }
     }
 };
 
 class crosswords extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+          "rSelected": "",
+          "setRSelected": "",
+          "response": "",
+        };
+        this.correct = this.correct.bind(this);
+      }
+    correct(){
+        alert("félications");
+        document.write("félicitations")
+    }
     render() {
         return (
             <Card body inverse /*color="success"*/ style={{ position: "center", width: "1350px", "background": "#2F8DD6" }}>
@@ -81,10 +94,11 @@ class crosswords extends Component {
                     <Row>
                         <Col xs="8">
                             <CardText style={{   "marginLeft":"400px", color:"black" }}>
-                                <Crossword data={data}  />
+                                <Crossword data={data} onCrosswordCorrect={this.correct}  />
                             </CardText>
                         </Col>
                     </Row>
+                    {/* <Button onclick={fillAllAnswers()}>Correct answers</Button> */}
                 </Container>
 
             </Card>
